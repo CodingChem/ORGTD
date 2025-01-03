76ba14aa6ef88a34c6c18d7504ab098f01db36a1
Vegard Pareli Seines <Vegard.Seines@outlook.com>
2025-01-03 13:19:39 +0100

    Add OrgNode and OrgTitle classes
    Added OrgNode class in OrgNode.cs to represent a node in an Org-mode document with properties and methods for managing title, content, tags, and child nodes. Added OrgTitle class in OrgTitle.cs to handle the title of an Org-mode node, including parsing, modifying the title, managing tags, and identifying tasks.

---
+++ src/ORGTD.OrgMode/OrgTitle.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ORGTD.OrgMode;
+
+
+internal class OrgTitle
+{
+	public OrgTitle(string raw)
+	{
+		_raw = raw;
+	}
+	private string _raw;
+	public string title
+	{
+		get => Regex.Replace(strip_task_keys(_raw.TrimStart('*')), OrgConfig.trim_tags_regex, "").Trim();
+		set => _raw.Replace(title, value);
+	}
+	public int level
+	{
+		get => _raw.TakeWhile(c => c == '*').Count();
+		set => _raw = new string('*', value) + _raw.TrimStart('*');
+	}
+
+	public List<string> Tags()
+	{
+		return Regex.Matches(_raw, OrgConfig.tags_regex).Select(m => m.Value).ToList();
+	}
+	public void AddTag(string tag)
+	{
+		_raw = _raw.TrimEnd() + " :" + tag + ":";
+	}
+	public void RemoveTag(string tag)
+	{
+		_raw = Regex.Replace(_raw, $":{tag}:", "");
+	}
+	public bool IsTask()
+	{
+		return OrgConfig.Instance.TaskKeys.Any(key => _raw.Contains(key));
+	}
+	public string Task
+	{
+		get => IsTask() ? TaskType() : "";
+		set
+		{
+			if (IsTask())
+			{
+				_raw = _raw.Replace(TaskType(), value);
+			}
+			else
+			{
+				// Insert task type after level
+				_raw = _raw.Insert(level, value + " ");
+			}
+		}
+	}
+	private string TaskType()
+	{
+		if (!IsTask()) return "";
+		return OrgConfig.Instance.TaskKeys.FirstOrDefault(key => _raw.Contains(key))!;
+	}
+	private string strip_task_keys(string title)
+	{
+		foreach (var key in OrgConfig.Instance.TaskKeys)
+		{
+			title = title.Replace(key, "");
+		}
+		return title;
+	}
+}
